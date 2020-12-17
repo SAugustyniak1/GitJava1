7a93db0530da9579b46d475dcef0eb6efa38bb1a
SAugustyniak1 <74648341+SAugustyniak1@users.noreply.github.com>
2020-12-17 11:58:24 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("hello World");
+        
+        System.out.println("2 things same time");
 
         System.out.println("another someting");
 
